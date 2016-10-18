b424f622e8323d9e259691f949aef59d0323012b
José Neto <jbsneto18@gmail.com>
2016-10-18 00:14:06 -0300

    Ajustes no form de cadastro de serviço

--- application/views/maps_register.php
+++ application/views/maps_register.php
@@ -11,79 +11,12 @@
         <title>Places Searchbox</title>
     </head>
     <body>
-        <div class="container">
+        <div class="container" style="height:50%;">
             <h2>Cadastro de serviços</h2>
             <br>
             <form>
                 <div class = "row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Endereço:</label>
-                            <input type="text" class="form-control" id="endereco">
-                        </div>
-                    </div>
                     <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Número:</label>
-                            <input type="text" class="form-control" id="numero">
-                        </div>
-                    </div>
-                </div>
-
-                <div class = "row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Complemento:</label>
-                            <input type="text" class="form-control" id="complemento">
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Bairro:</label>
-                            <input type="text" class="form-control" id="bairro">
-                        </div>
-                    </div>
-                </div>
-
-                <div class = "row">
-                    <div class="col-md-3">
-                        <div class="form-group">
-                            <label>Cidade:</label>
-                            <select class="selectpicker" data-width="100%">
-                                <option>Itajuípe</option>
-                                <option>Coaraci</option>
-                                <option>Itapitanga</option>
-                                <option>Itabuna</option>
-                                <option>Buerarema</option>
-                                <option>Ilhéus</option>
-                            </select>
-
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="form-group">
-                            <label>Estado:</label>
-                            <select class="selectpicker" data-width="100%">
-                                <option>Acre</option>
-                                <option>Amazonas</option>
-                                <option>Alagoas</option>
-                                <option>Bahia</option>
-                                <option>Ceará</option>
-                                <option>Distrito Federal</option>
-                            </select>
-
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label>CEP:</label>
-                            <input type="text" class="form-control" id="cep">
-                        </div>
-                    </div>
-                </div>
-
-                <div class = "row">
-                    <div class="col-md-3">
                         <div class="form-group">
                             <label>Serviço:</label>
                             <div class="select_service">
@@ -99,22 +32,83 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-7">
+                    <div class="col-md-8">
                         <div class="form-group">
                             <label>Descrição:</label>
                             <input type="text" class="form-control" id="descricao">
                         </div>
                     </div>
                 </div>
-
                 <div class = "row">
                     <div class="col-md-5">
+                        <div class="form-group">
+                            <label>Endereço:</label>
+                            <input type="text" class="form-control" id="endereco">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label>Número:</label>
+                            <input type="text" class="form-control" id="numero">
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="form-group">
+                            <label>Complemento:</label>
+                            <input type="text" class="form-control" id="complemento">
+                        </div>
+                    </div>
+                </div>
+
+                <div class = "row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Bairro:</label>
+                            <input type="text" class="form-control" id="bairro">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Cidade:</label>
+                            <select class="selectpicker" data-width="100%">
+                                <option>Itajuípe</option>
+                                <option>Coaraci</option>
+                                <option>Itapitanga</option>
+                                <option>Itabuna</option>
+                                <option>Buerarema</option>
+                                <option>Ilhéus</option>
+                            </select>
+
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Estado:</label>
+                            <select class="selectpicker" data-width="100%">
+                                <option>Acre</option>
+                                <option>Amazonas</option>
+                                <option>Alagoas</option>
+                                <option>Bahia</option>
+                                <option>Ceará</option>
+                                <option>Distrito Federal</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class = "row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>CEP:</label>
+                            <input type="text" class="form-control" id="cep">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
                         <div class="form-group">
                             <label>Latitude:</label>
                             <input type="text" class="form-control" id="latitude">
                         </div>
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-md-4">
                         <div class="form-group">
                             <label>Longitude:</label>
                             <input type="text" class="form-control" id="longitude">
@@ -122,11 +116,21 @@
                     </div>
                 </div>
             </form>
+            <div class="col-md-12" style="height: 100%;">
+                <!-- BEGIN MAPA -->
+                <div id="map"></div>
+                <input id="pac-input" class="controls" type="text" placeholder="Pesquisar">
+                <!-- END MAPA-->
+            </div>
+            <div class = "row">
+                <div class="col-md-1" style="margin-top: 30px; float:right;">
+                    <button type="submit" class="btn btn-success">Cadastrar</button>
+                </div>
+                <div class="col-md-1" style="margin-top: 30px; float:right;">
+                    <button type="button" class="btn btn-danger">Cancelar</button>
+                </div>
+            </div>
         </div>
-        <!-- BEGIN MAPA -->
-        <div id="map"></div>
-        <input id="pac-input" class="controls" type="text" placeholder="Pesquisar">
-        <!-- END MAPA-->
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC69Ji81pHJ6ol7VhIrIDE1mUofcZw_WuA&libraries=places&callback=initAutocomplete" async defer></script>
         <script src="<?= base_url('/assets/js/google_maps/maps_register.js'); ?>" type="text/javascript"></script>
         <link href="<?= base_url('/assets/css/google_maps/maps_register.css'); ?>" rel="stylesheet" type="text/css" />
