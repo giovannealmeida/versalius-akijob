f87719c279229d4f8486370abc3e4fc01771b78e
Filipe_Drisostes <filipe.drisostes@gmail.com>
2016-11-29 03:04:34 -0300

    Modificação na exibição das estatisticas. Acrescimo de 'os ultimos visitantes'. Faltam melhorias, tais como, exibir os visitantes por mês, colocar as fotos de cada visitante, dentre outras

--- application/views/profile/menu.php
+++ application/views/profile/menu.php
@@ -55,7 +55,7 @@
                         </li>
                         <?php if ($premium_data["isPremium"]): ?>
                             <li>
-                                <a href="<?= base_url("profile/statistics/".date('m').'-'.date('Y')) ?>">
+                                <a href="<?= base_url("profile/statistics/") ?>">
                                     <i class="fa fa-line-chart" aria-hidden="true"></i> Estatísticas
                                     <span class="label label-warning">Em Breve!</span>
                                 </a>
